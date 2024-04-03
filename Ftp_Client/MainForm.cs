fa3f8c7d068e47386039d74a49aff92651272aed
S0me0ne24 <thuyvo.191104@gmail.com>
2024-04-03 12:21:30 +0700

    login

--- Ftp_Client/MainForm.cs
+++ Ftp_Client/MainForm.cs
@@ -16,23 +16,6 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace Ftp_Client
 {
-    public class Account
-    {
-        public ObjectId _id { get; set; }
-
-        [BsonElement("username")]
-        public string username { get; set; }
-
-        [BsonElement("passwd")]
-        public string passwd { get; set; }
-
-        public Account(string _username, string _passwd)
-        {
-            this.username = _username;
-            this.passwd = _passwd;
-        }
-
-    }
     public partial class MainForm : Form
     {
         public MainForm()
@@ -157,6 +140,23 @@ namespace Ftp_Client
          * Upload nam trong LocalSiteForm
          * Download nam trong RemoteSiteForm
          */
+
+    }
+    public class Account
+    {
+        public ObjectId _id { get; set; }
+
+        [BsonElement("username")]
+        public string username { get; set; }
+
+        [BsonElement("passwd")]
+        public string passwd { get; set; }
+
+        public Account(string _username, string _passwd)
+        {
+            this.username = _username;
+            this.passwd = _passwd;
+        }
 
     }
 }
